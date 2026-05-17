718f0e0819a25a21d1750c5673590d6ecd53c93a
rk1309 <91061821+rk1309@users.noreply.github.com>
2026-05-17 17:06:20 +0530

    Task 1

---
+++ Task 1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+text = "C++ is Powerful"
+
+
+# In[11]:
+
+
+reversed_text = " ".join(text.split()[::-1])
+
+
+# In[12]:
+
+
+print(reversed_text)
+
+
+# In[ ]:
+
+
+
+
